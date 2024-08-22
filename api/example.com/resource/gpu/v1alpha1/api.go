c1fba136d3acb0c3ade611bdb05c1561758f708f
Kevin Klues <kklues@nvidia.com>
2024-08-22 23:14:13 +0200

    Update objects in example.com API group to only define opaque GPU config
    Signed-off-by: Kevin Klues <kklues@nvidia.com>

--- api/example.com/resource/gpu/v1alpha1/api.go
+++ api/example.com/resource/gpu/v1alpha1/api.go
@@ -17,32 +17,94 @@
 package v1alpha1
 
 import (
-	"k8s.io/utils/ptr"
+	"fmt"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 )
 
 const (
 	GroupName = "gpu.resource.example.com"
 	Version   = "v1alpha1"
 
-	GpuDeviceType     = "gpu"
-	UnknownDeviceType = "unknown"
-
-	GpuClaimParametersKind = "GpuClaimParameters"
+	GpuConfigKind = "GpuConfig"
 )
 
-func DefaultDeviceClassParametersSpec() *DeviceClassParametersSpec {
-	return &DeviceClassParametersSpec{
-		DeviceSelector: []DeviceSelector{
-			{
-				Type: GpuDeviceType,
-				Name: "*",
+// Decoder implements a decoder for objects in this API group.
+var Decoder runtime.Decoder
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// GpuConfig holds the set of parameters for configuring a GPU.
+type GpuConfig struct {
+	metav1.TypeMeta `json:",inline"`
+	Sharing         *GpuSharing `json:"sharing,omitempty"`
+}
+
+// DefaultGpuConfig provides the default GPU configuration.
+func DefaultGpuConfig() *GpuConfig {
+	return &GpuConfig{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: GroupName + "/" + Version,
+			Kind:       GpuConfigKind,
+		},
+		Sharing: &GpuSharing{
+			Strategy: TimeSlicingStrategy,
+			TimeSlicingConfig: &TimeSlicingConfig{
+				Interval: "Default",
 			},
 		},
 	}
 }
 
-func DefaultGpuClaimParametersSpec() *GpuClaimParametersSpec {
-	return &GpuClaimParametersSpec{
-		Count: ptr.To(1),
+// Normalize updates a GpuConfig config with implied default values based on other settings.
+func (c *GpuConfig) Normalize() error {
+	if c == nil {
+		return fmt.Errorf("config is 'nil'")
 	}
+	if c.Sharing == nil {
+		c.Sharing = &GpuSharing{
+			Strategy: TimeSlicingStrategy,
+		}
+	}
+	if c.Sharing.Strategy == TimeSlicingStrategy && c.Sharing.TimeSlicingConfig == nil {
+		c.Sharing.TimeSlicingConfig = &TimeSlicingConfig{
+			Interval: "Default",
+		}
+	}
+	if c.Sharing.Strategy == SpacePartitioningStrategy && c.Sharing.SpacePartitioningConfig == nil {
+		c.Sharing.SpacePartitioningConfig = &SpacePartitioningConfig{
+			PartitionCount: 1,
+		}
+	}
+	return nil
+}
+
+func init() {
+	// Create a new scheme and add our types to it. If at some point in the
+	// future a new version of the configuration API becomes necessary, then
+	// conversion functions can be generated and registered to continue
+	// supporting older versions.
+	scheme := runtime.NewScheme()
+	schemeGroupVersion := schema.GroupVersion{
+		Group:   GroupName,
+		Version: Version,
+	}
+	scheme.AddKnownTypes(schemeGroupVersion,
+		&GpuConfig{},
+	)
+	metav1.AddToGroupVersion(scheme, schemeGroupVersion)
+
+	// Set up a json serializer to decode our types.
+	Decoder = json.NewSerializerWithOptions(
+		json.DefaultMetaFactory,
+		scheme,
+		scheme,
+		json.SerializerOptions{
+			Pretty: true, Strict: true,
+		},
+	)
 }
