c8f7d6d2273a8c35c7a47ed47adec38d5a570d0a
svarnam <svarnam@nvidia.com>
2026-06-11 22:31:45 +0530

    Add support for featureGates
    Signed-off-by: svarnam <svarnam@nvidia.com>

--- pkg/flags/logging.go
+++ pkg/flags/logging.go
@@ -22,9 +22,10 @@ import (
 	"github.com/spf13/pflag"
 	"github.com/urfave/cli/v2"
 
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/component-base/featuregate"
 	logsapi "k8s.io/component-base/logs/api/v1"
+
+	"sigs.k8s.io/dra-example-driver/pkg/featuregates"
 
 	_ "k8s.io/component-base/logs/json/register" // for JSON log output support
 )
@@ -35,15 +36,10 @@ type LoggingConfig struct {
 }
 
 func NewLoggingConfig() *LoggingConfig {
-	fg := featuregate.NewFeatureGate()
-	var _ pflag.Value = fg // compile-time check for the type conversion below
-	l := &LoggingConfig{
-		featureGate: fg,
+	return &LoggingConfig{
+		featureGate: featuregates.FeatureGates(),
 		config:      logsapi.NewLoggingConfiguration(),
 	}
-	utilruntime.Must(logsapi.AddFeatureGates(l.featureGate))
-	utilruntime.Must(l.featureGate.SetFromMap(map[string]bool{string(logsapi.ContextualLogging): true}))
-	return l
 }
 
 // Apply should be called in a cli.App.Before directly after parsing command
@@ -61,12 +57,7 @@ func (l *LoggingConfig) Flags() []cli.Flag {
 	// with "logging" as category. In practice, the logging code is the
 	// only code which uses the flag, therefore that seems like a good
 	// place to report it.
-	fs.AddFlag(&pflag.Flag{
-		Name: "feature-gates",
-		Usage: "A set of key=value pairs that describe feature gates for alpha/experimental features. " +
-			"Options are:\n     " + strings.Join(l.featureGate.KnownFeatures(), "\n     "),
-		Value: l.featureGate.(pflag.Value), //nolint:forcetypeassert // No need for type check: l.featureGate is a *featuregate.featureGate, which implements pflag.Value.
-	})
+	l.featureGate.AddFlag(&fs)
 
 	var flags []cli.Flag
 	fs.VisitAll(func(flag *pflag.Flag) {
